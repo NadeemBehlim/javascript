134a99950421322929059cdee69940176d1b48ef
NadeemBehlim <124064810+NadeemBehlim@users.noreply.github.com>
2023-03-11 10:34:52 +0530

    Add files via upload

---
+++ arrayexe.js
@@ -0,0 +1,125 @@
+//question no.1 Create an array with three elements and print out the second element.
+//let arr = [1, 2, 3];
+//console.log (arr[2]);
+
+//question no. 2=Create an array with five elements and print out the length of the array.
+//let arr = [1, 2, 3, 4, 5];
+//console.log (arr.length);
+
+//question no.3 = Create an array with four elements and print out each element using a for loop
+//let arr = [1,2,3,4,5];
+//for (let i = 0; i<arr.length; i++){
+//console.log(arr[i])
+//}
+
+//question No. 4=Create an array with six elements and print out each element using a forEach loop.
+//arr = [1,2,3,4,5,6];
+//for(let arr1 in arr){
+//    console.log(arr[arr1]);
+//}
+
+//question no.5 = Create an array with three elements and add a fourth element to the end of the array.
+//arr = [1,2,3] 
+//arr.push(4);
+//console.log(arr);
+
+//question No.6 =Create an array with four elements and remove the second element.
+//arr = [1,2,3,4]
+//arr.splice(1,1);
+//console.log(arr);
+
+//question no. 7 Create an array with five elements and remove the last element.
+//arr = [1,2,3,4,5,6,7]
+//arr.pop();
+//console.log(arr);
+
+//question no . 8 Create an array with three elements and check if the array includes a specific value.
+
+//let arr = [1,2,3,4];
+//let newArr = arr.includes(4);
+//console.log(newArr);
+
+//question No. 9=Create an array with four elements and sort the array in ascending order.
+//let arr = [1, 3, 6, 9, 8];
+//let newArr = arr.sort(test);
+//function test(a, b) {
+//  return a - b;
+//}
+//console.log(newArr);
+
+
+//question No. 10 =Create an array with five elements and sort the array in descending order.
+//let arr = [1, 3, 6, 9, 8];
+//let newArr = arr.sort(test);
+//function test(a, b) {
+//  return b - a;
+//}
+//console.log(newArr);
+
+
+// Q.11.Create two arrays, concatenate them and print out the resulting array.
+//let arr = [1, 2, 3, 4,];
+//let arr1 = [5, 6, 7, 8];
+//let newArr = arr.concat(arr1);
+//console.log(newArr);
+
+// Q.12.Create an array with three elements and convert it to a string.
+//let arr = ["nadeem", 1, 2];
+//let newArr = arr.toString();
+//console.log(newArr);
+
+
+//Q.13.Create an array with four elements and reverse the order of the elements.
+//let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//arr.reverse();
+//console.log(arr);
+
+
+//Q.14.Create an array with five elements and find the index of a specific value.
+//let arr = [1, 2, 3, 4, 5];
+//arr.indexOf();
+//console.log(arr);
+
+
+//Q.15.Create an array with six elements and slice the array to create a new array with the first three elements.
+//let arr = [1, 2, 3, 4, 5, 6];
+//let newArr = arr.slice(0, 3);
+//console.log(newArr);
+
+//Q.16.Create an array with six elements and use the map method to double each element.
+//let arr = [1, 2, 3, 4, 5, 6];
+//let newArr = arr.map(test);
+//function test(num) {
+//  return num * 2;
+//}
+//console.log(newArr);
+
+//Q.17.Create an array with four elements and use the while loop to calculate the sum of all elements.
+//let i = 0;
+//let arr = [1, 2, 3, 4];
+//let sum = 0;
+//while (i < arr.length) {
+//  sum = sum + arr[i];
+//  i++;
+//}
+//console.log(sum);
+
+//Q.18.Create an array with five elements and use the filter method to return only the even numbers.
+//let arr = [1, 2, 3, 4, 5];
+//let newArr = arr.filter(test);
+//function test(num) {
+//  return num % 2 === 0;
+//}
+//console.log(newArr);
+
+//Q.19.Create an array with three elements and use the join method to concatenate the elements with a dash (-) separator.
+//let arr = [1, 3, 5];
+//let newArr = arr.join("-");
+//console.log(newArr);
+
+
+//Q.20.Create two arrays with three elements each and use the concat method to combine them into a new array.
+//let arr = [1, 2, 3];
+//let arr1 = [4, 5, 6];
+//let newArr = arr.concat(arr1);
+//console.log(newArr);
